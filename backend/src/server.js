64ad7fc3f12c7023eb30770d99f8b3c6198990c1
RogerGTWARR <rperezcastillo912@gmail.com>
2025-08-23 04:24:42 -0600

    First Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hello', (req, res) => {
-  res.json({ msg: 'Hola desde backend' });
+  res.json({ msg: 'Hola' });
 });
 
 app.listen(3000, () => console.log('Backend en http://localhost:3000'));
