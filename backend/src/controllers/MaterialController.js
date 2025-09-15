caa3dcd1d5fbaae2ad10dd5b8112d0bd5e169b02
Roger Osorio <rogermoises2004@gmail.com>
2025-09-15 12:32:55 -0600

    getAll and getById materials

--- backend/src/controllers/MaterialController.js
+++ backend/src/controllers/MaterialController.js
@@ -6,7 +6,40 @@ export default class MaterialController {
 
     const materials = await prisma.materials.findMany();
 
-    res.json(materials);
+    res.json({
+      ok: true,
+      data: materials
+    });
+  }
+
+  static async getById(req, res) {
+
+    const { id } = req.params;
+
+    const materialId = parseInt(id);
+
+    if (isNaN(materialId)) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El id de material debe ser un numero"
+      })
+    }
+
+    const material = await prisma.materials.findUnique({
+      where: { id: materialId }
+    });
+
+    if (material === null) {
+      return res.status(404).json({
+        ok: false,
+        msg: `No se encontro el material con id: ${materialId}`
+      })
+    }
+
+    res.json({
+      ok: true,
+      data: material
+    });
   }
 
 }
--- backend/src/routes/materialRoutes.js
+++ backend/src/routes/materialRoutes.js
@@ -4,5 +4,6 @@ import MaterialController from "../controllers/MaterialController.js";
 const materialRouter = express.Router();
 
 materialRouter.get('/', MaterialController.getAll);
+materialRouter.get('/{:id}', MaterialController.getById);
 
 export default materialRouter;
