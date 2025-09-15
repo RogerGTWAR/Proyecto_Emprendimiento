54777bf32ece52581f6612bb0e3c58cd31ccc5d7
Roger Osorio <rogermoises2004@gmail.com>
2025-09-15 10:56:06 -0600

    setting up database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from 'express';
+import prisma from './database.js';
+
 const app = express();
 
-app.get('/api/hello', (req, res) => {
-  res.json({ msg: 'Hola' });
+app.get('/api/hello', async (req, res) => {
+
+  const users = await prisma.users.findMany();
+
+  res.json(users);
+
 });
 
 app.listen(3000, () => console.log('Backend en http://localhost:3000'));
